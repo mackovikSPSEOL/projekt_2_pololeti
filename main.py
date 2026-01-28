9e52722b1a10eeb37a6f4b3548647d57aabea6f1
Matyáš <mac42396@spseol.cz>
2026-01-28 11:15:08 +0100

    scio testy jsou zlo

---
+++ main.py
@@ -0,0 +1,19 @@
+import random
+import customtkinter as ctk
+import tkinter.font as tkfont
+
+main_window = ctk.CTk()
+
+main_window
+
+main_window = ctk.CTk()
+
+main_window.title("Educational App")
+
+main_window.minsize(900,400)
+
+
+
+
+
+main_window.mainloop()
